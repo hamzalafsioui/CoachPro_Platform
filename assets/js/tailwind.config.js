d60e0149a3f6ba662d3fcd7ff419ca9db6c7314a
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-16 23:07:45 +0100

    global tailwindCss configurations

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,13 @@
+// Global Tailwind CSS Configuration
+
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3b82f6",
+        secondary: "#06b6d4",
+        accent: "#8b5cf6",
+      },
+    },
+  },
+};
