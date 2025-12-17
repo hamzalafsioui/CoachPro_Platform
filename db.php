894d89a6c7ace9aa878a5d3edd6a70fdd3a88ae5
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-17 12:59:10 +0100

    db connection Mysqli

---
+++ db.php
@@ -0,0 +1,19 @@
+<?php
+// db.php - Global database connection
+
+// Database credentials
+$host = 'localhost';
+$user = 'root';
+$pass = 'Sa@123456';
+$dbname = 'coach_pro';
+
+// Create MySQLi connection
+$conn = new mysqli($host, $user, $pass, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Database connection failed: " . $conn->connect_error);
+}
+
+// Set charset to utf8
+$conn->set_charset("utf8");
