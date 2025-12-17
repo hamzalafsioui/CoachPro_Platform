b4a279ece2f1fb4532a17d28c89d9bad11df09ec
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-17 21:34:06 +0100

    Global Tailwind CSS Configuration

--- assets/js/tailwind.config.js
+++ assets/js/tailwind.config.js
@@ -3,10 +3,19 @@
 tailwind.config = {
   theme: {
     extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        outfit: ["Outfit", "sans-serif"],
+      },
       colors: {
         primary: "#3b82f6",
         secondary: "#06b6d4",
         accent: "#8b5cf6",
+      },
+      dark: {
+        900: "#0f172a",
+        800: "#1e293b",
+        700: "#334155",
       },
     },
   },
