67965ff5eb27f2af0063a0d22aba8e86eb6c8b61
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-23 14:42:04 +0100

    DB class

---
+++ classes/Database.php
@@ -0,0 +1,32 @@
+<?php
+
+class Database
+{
+    private static ?PDO $instance = null;
+
+    public static function getInstance(): PDO
+    {
+        if (self::$instance === null) {
+            try {
+                
+                $host = defined('DB_HOST') ? DB_HOST : 'localhost';
+                $user = defined('DB_USER') ? DB_USER : 'root';
+                $pass = defined('DB_PASS') ? DB_PASS : 'Sa@123456';
+                $name = defined('DB_NAME') ? DB_NAME : 'coachPro';
+
+                $dsn = "mysql:host={$host};dbname={$name};charset=utf8mb4";
+
+                self::$instance = new PDO($dsn, $user, $pass, [
+                    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                    PDO::ATTR_EMULATE_PREPARES   => false,
+                ]);
+            } catch (PDOException $e) {
+                error_log("Database Connection Error: " . $e->getMessage());
+                throw new Exception("Could not connect to the database.");
+            }
+        }
+
+        return self::$instance;
+    }
+}
