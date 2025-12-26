a61e51cf812b59763690c8172849d3fc52f3177d
hamzalafsioui <hamzalafsioui@gmail.com>
2025-12-26 10:42:05 +0100

    challenge02

--- challenges_sql_coach/challenge02.sql
+++ challenges_sql_coach/challenge02.sql
@@ -7,8 +7,21 @@
 -- ordre décroissant par nombre de réservations
 -- À utiliser : JOIN, GROUP BY, DATE_FORMAT, ORDER BY
 
+SELECT * FROM reservations;
+SELECT * FROM seances;
+SELECT * FROM sportifs;
+SELECT * FROM users
 
 
+SELECT 
+    u.nom,
+    u.prenom,
+    COUNT(r.id) as nbr_res,
+    DATE_FORMAT(r.reserved_at,'%m-%Y') as month_year
 
--- 01 nom, prénom
+FROM reservations r
+INNER JOIN users u ON u.id = r.sportif_id
+WHERE u.role = 'sportif'
+GROUP BY u.id,month_year
+ORDER BY nbr_res DESC;
 
